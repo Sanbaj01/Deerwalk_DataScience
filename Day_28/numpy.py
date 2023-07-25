cc0c86dfb26844c90f363a9ff845a9ee6643dfc2
Sanbaj01 <sanbajansari01@gmail.com>
2023-07-25 15:35:31 +0545

    notes on numpy added

---
+++ Day_28/numpy.py
@@ -0,0 +1,29 @@
+#Numpy - Numerical Python
+'''
+Python Vs C 
+Python is slow and productive.
+C is fast.
+So, Numpy is a mixture of both to maintain productivity and fastness.
+Numpy is itself made by C and runs in python.
+'''
+
+''''
+Statistics
+inferential statistics
+Differential statistics - mesure of central tendency
+
+
+Correltion 
+-Always between 1 and -1.
+> 0.6 positive correlation      
+< -0.6 negative correlation
+-0.6 between 0.6 does not matter much, very less correlation.
+
+Slope (tanx) --> rate of change
+
+slope of non-linear equation --> Derivative (x^3)
+
+
+
+'''
+
