23cb8d176737646585fe34f7290d0ebdc384360f
Sanbaj01 <sanbajansari01@gmail.com>
2023-07-31 19:34:24 +0545

    numpy examples added

--- Day_28/numpy.py
+++ Day_28/numpy.py
@@ -44,3 +44,11 @@ Array - elemens are store in contiguous storage(one after another).
 
 #I need to strengten my DSA knowledge, to enhance my skills.
 ########### CHECK OUT LINKED LIST.
+import numpy as np
+
+0 * np.nan
+np.nan == np.nan
+np.inf > np.nan
+np.nan - np.nan
+np.nan in set([np.nan])
+0.3 == 3 * 0.1
