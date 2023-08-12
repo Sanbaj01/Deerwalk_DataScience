e3222dba00e3c9c26968eb441d5cfc439b0221c2
Sanbaj01 <sanbajansari01@gmail.com>
2023-08-12 08:54:24 +0545

    files updated

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-i = 5
-result = type(i)
-print(result)
+'''
+10. Problem-Solving:
+a. Given an array of integers, write an algorithm to find the two numbers that add up to a specific target sum.
+b. Solve the classic FizzBuzz problem: Print numbers from 1 to 100, but replace multiples of 3 with "Fizz" and multiples of 5 with "Buzz."
+'''
+# Fizzbuzz problem
+a = list(range(1,101))
+for i in range(len(a)):
+    if a[i] % 3 ==0 and a[i] % 5 == 0:
+        a[i] = 'Fizzbuzz'
+    elif a[i] % 3 == 0:
+        a[i] = 'Fizz'
+    elif a[i] % 5 == 0:
+        a[i] = 'buzz'
+print(a)
